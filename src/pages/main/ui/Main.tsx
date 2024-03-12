e2d08a188d88f23eddcea0848b3b5ab5f107e97b
Стрельников Максим Дмитриевич <mstrelnikov@nic.etu>
2024-03-12 12:57:45 +0300

    api

--- src/pages/main/ui/Main.tsx
+++ src/pages/main/ui/Main.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
+import { GetGroups } from '../api/index'
 
 export function Main() {
 
 	//const [groups, setGroups] = useState<GetGroupsResponse[]>()
 
 	useEffect(() => {
-		fetch('http://localhost:3001/groups')
-			.then(res => res.json())
+		GetGroups("http://localhost:3001/groups")
 			.then(data => console.log(data))
 	}, [])
 
